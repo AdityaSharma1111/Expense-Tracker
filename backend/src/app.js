42256162a9b1cb73ae2fc7f8db758ec376808899
AdityaSharma <adityasharma11nov@gmail.com>
2025-04-14 14:43:14 +0530

    refactor: remove directory creation logic for public/temp from app initialization

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,24 +6,6 @@ import cookieParser from "cookie-parser";
 import expenseRouter from './routes/expense.routes.js'
 import dashboardRouter from './routes/dashboard.routes.js'
 
-
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Needed to get __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Ensure 'public/temp' directory exists
-const tempDir = path.join(__dirname, "public", "temp");
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true });
-  console.log("✅ Created public/temp directory");
-}
-
-
-
 const app = express();
 
 app.use(cors({
@@ -35,7 +17,6 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true, limit: "16kb" }));
-
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/income", incomeRouter);
@@ -56,7 +37,6 @@ app.use((err, req, res, next) => {
         stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
     });
 });
-
 
 
 export { app };
